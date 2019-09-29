7d99437224da43af66e1d049b99b350f7779dbd0
zhengchaobin <zhengchaobin@yunnex.com>
2019-09-29 11:34:42 +0800

    update custom error

--- ErrorTest.go
+++ ErrorTest.go
@@ -17,12 +17,20 @@ func main() {
 	}
 
 
-	// 异常测试
-	num, e := customErrorTest(0)
+	// 异常自定义测试
+	num, e := customErrorTest(1)
 	if e != nil{
 		fmt.Println(num, e.Error())
 	}else {
 		fmt.Println(num)
+	}
+
+	// 异常测试
+	num2, e := customErrorTest(0)
+	if e != nil{
+		fmt.Println(num2, e.Error())
+	}else {
+		fmt.Println(num2)
 	}
 
 
@@ -36,15 +44,15 @@ func errorTest(num int) (int, error) {
 	return num, nil
 }
 
-func customErrorTest(num int) (int, *zcb.CustomError) {
+func customErrorTest(num int) (int, error) {
 
-	var e *zcb.CustomError
+	var e zcb.CustomError
 	if num < 1{
-		e := zcb.New("自定义异常")
+		e = zcb.New("自定义异常")
 		return num, e
 	}
-	// 正常情况下error返回nil
-	return num, e
+	// 正常情况下error返回nil,注意: 不能返回e,返回e的话会非nil
+	return num, nil
 }
 
 
